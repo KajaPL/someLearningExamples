b7a9facb286f8455b0ca7fdd19e54f75a62dd634
kajam <caia.balcania@gmail.com>
2023-01-08 16:10:11 +0100

    mac abstract programiz

---
+++ src/main/java/Abstract/Abstract2.java
@@ -0,0 +1,30 @@
+package Abstract;
+
+abstract class MotorBike {
+    abstract void brake();
+}
+
+class SportsBike extends MotorBike {
+
+    // implementation of abstract method
+    public void brake() {
+        System.out.println("SportsBike Brake");
+    }
+}
+
+class MountainBike extends MotorBike {
+
+    // implementation of abstract method
+    public void brake() {
+        System.out.println("MountainBike Brake");
+    }
+}
+
+class Main1 {
+    public static void main(String[] args) {
+        MountainBike m1 = new MountainBike();
+        m1.brake();
+        SportsBike s1 = new SportsBike();
+        s1.brake();
+    }
+}
